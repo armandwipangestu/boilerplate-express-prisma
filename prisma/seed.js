02f300c2f38836a57d8295e1e5171fe0495b2d4a
armandwipangestu <armandwi.pangestu7@gmail.com>
2024-04-23 14:54:41 +0700

    feat: add posts seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,6 @@
 import prisma from "../db/prisma.js";
 import { clean } from "./seed/helper/clean.js";
+import { createPostsOfUser } from "./seed/helper/PostsOfUser.js";
 import { createUserWithProfile } from "./seed/helper/UserWithProfile.js";
 
 async function main() {
@@ -8,7 +9,8 @@ async function main() {
     console.log(`Database cleaned.`);
 
     console.log(`Start seeding ...`);
-    console.log(`Seeding createdUserWithProfile`);
+
+    console.log(`Seeding createUserWithProfile`);
     await createUserWithProfile(10)
         .then((usersWithProfiles) => {
             console.log(`Seeder createUserWithProfile success!`);
@@ -16,6 +18,16 @@ async function main() {
         .catch((error) => {
             console.log(`Seeder createdUserWithProfile error: ${error}`);
         });
+
+    console.log(`Seeding createPostsOfUser`);
+    await createPostsOfUser(10)
+        .then((postsOfUsers) => {
+            console.log(`Seeder createPostsOfUser success!`);
+        })
+        .catch((error) => {
+            console.log(`Seeder createPostsOfUser error: ${error}`);
+        });
+
     console.log(`Seeding finished.`);
 }
 
